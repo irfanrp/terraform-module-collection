2e896ee5b97d146b439c9b7775561fe389679b02
Irfan Rizkianto Pratama <idsta.irfanrizkianto@xl.co.id>
2025-10-05 23:57:21 +0700

    chore: Format code in RDS module for improved readability

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,22 +1,22 @@
 locals {
   subnet_group_needed = var.db_subnet_group_name == null && length(var.db_subnet_ids) > 0
-  subnet_group_name = var.db_subnet_group_name != null ? var.db_subnet_group_name : "${var.identifier}-subnet-group"
+  subnet_group_name   = var.db_subnet_group_name != null ? var.db_subnet_group_name : "${var.identifier}-subnet-group"
 }
 
 resource "aws_db_subnet_group" "this" {
   count = local.subnet_group_needed ? 1 : 0
 
-  name       = local.subnet_group_name
-  subnet_ids = var.db_subnet_ids
+  name        = local.subnet_group_name
+  subnet_ids  = var.db_subnet_ids
   description = "DB subnet group for ${var.identifier}"
-  tags = var.tags
+  tags        = var.tags
 }
 
 resource "aws_db_instance" "this" {
-  identifier = var.identifier
-  engine     = var.engine
-  engine_version = var.engine_version
-  instance_class = var.instance_class
+  identifier        = var.identifier
+  engine            = var.engine
+  engine_version    = var.engine_version
+  instance_class    = var.instance_class
   allocated_storage = var.allocated_storage
 
   username = var.username
