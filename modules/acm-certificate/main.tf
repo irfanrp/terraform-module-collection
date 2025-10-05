9f798032f5cbb6deceace712d928bf07f7169f15
Irfan Rizkianto Pratama <idsta.irfanrizkianto@xl.co.id>
2025-10-06 00:25:17 +0700

    chore: Format code in ACM certificate module for improved readability

--- modules/acm-certificate/main.tf
+++ modules/acm-certificate/main.tf
@@ -2,20 +2,20 @@ resource "aws_acm_certificate" "this" {
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
   validation_method         = var.validation_method
-  tags = var.tags
+  tags                      = var.tags
 }
 
 resource "aws_acm_certificate" "us_east" {
-  count = var.for_cloudfront ? 1 : 0
-  provider = aws.use_east_1
+  count                     = var.for_cloudfront ? 1 : 0
+  provider                  = aws.use_east_1
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
   validation_method         = var.validation_method
-  tags = var.tags
+  tags                      = var.tags
 }
 
 data "aws_route53_zone" "zone" {
-  count = var.hosted_zone_id == null && var.zone_name != null ? 1 : 0
+  count        = var.hosted_zone_id == null && var.zone_name != null ? 1 : 0
   name         = var.zone_name
   private_zone = var.private_zone
 }
@@ -37,13 +37,13 @@ resource "aws_route53_record" "validation" {
 resource "aws_acm_certificate_validation" "this" {
   count = var.validation_method == "DNS" ? 1 : 0
 
-  certificate_arn = var.for_cloudfront && length(aws_acm_certificate.us_east) > 0 ? aws_acm_certificate.us_east[0].arn : aws_acm_certificate.this.arn
+  certificate_arn         = var.for_cloudfront && length(aws_acm_certificate.us_east) > 0 ? aws_acm_certificate.us_east[0].arn : aws_acm_certificate.this.arn
   validation_record_fqdns = var.create_route53_records && local.hosted_zone_id != null ? aws_route53_record.validation[*].fqdn : []
 }
 
 resource "aws_acm_certificate_validation" "us_east" {
-  count = var.for_cloudfront && var.validation_method == "DNS" ? 1 : 0
-  provider = aws.use_east_1
-  certificate_arn = aws_acm_certificate.us_east[0].arn
+  count                   = var.for_cloudfront && var.validation_method == "DNS" ? 1 : 0
+  provider                = aws.use_east_1
+  certificate_arn         = aws_acm_certificate.us_east[0].arn
   validation_record_fqdns = var.create_route53_records && local.hosted_zone_id != null ? aws_route53_record.validation[*].fqdn : []
 }
